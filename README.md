19478164fdf653368275da629a02e7c6f5febbe7
Dev <Dev@MacBook-Pro-de-AGOGUE-2.local>
2021-09-19 09:34:31 +0000

    enable actuator httptrace endpoint

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
   - Access ui => http://localhost:1000/swagger-ui.html
   - Access json content => http://localhost:1000/v2/api-docs
 - Actuator
-  - 
+  - Access actuator available endpoints through default endpoint => http://localhost:1000/actuator or
+through custom endpoint configured in app properties
--- src/main/java/com/dov/firstapi/FirstApiApplication.java
+++ src/main/java/com/dov/firstapi/FirstApiApplication.java
@@ -1,12 +1,20 @@
 package com.dov.firstapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
 public class FirstApiApplication {
+
+	@Bean
+	public HttpTraceRepository htttpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstApiApplication.class, args);
